e29dda4fedfc53e6cc3b00d5e779698094e18d52
Ivaylo Zhelev <ivaylo.zhelev.99@gmail.com>
2022-11-26 19:28:05 +0100

    Converting Tensors to scalars

--- denoising_diffusion_pytorch/evaluation.py
+++ denoising_diffusion_pytorch/evaluation.py
@@ -12,15 +12,15 @@ def iou(predicted, ground_truth, threshold=0.5):
 
     intersection = predicted.logical_and(ground_truth).sum()
     union = predicted.logical_or(ground_truth).sum()
-    return intersection / union
+    return (intersection / union).cpu()
 
 def dice(predicted, ground_truth, threshold=0.5):
     predicted = (predicted >= threshold).int()
     ground_truth = (ground_truth >= threshold).int()
-    return dice_score(predicted, ground_truth)
+    return dice_score(predicted, ground_truth).cpu()
 
 def ssim(predicted, ground_truth, *args, **kwargs):
-    return structural_similarity_index_measure(predicted, ground_truth)
+    return structural_similarity_index_measure(predicted, ground_truth).cpu()
 
 EVAL_FUNCTIONS = {
     IOU_METRIC: iou,
