06d19982762ff64d2c7f9a3f1ae402e5ad007fbf
Ivaylo Zhelev <ivaylo.zhelev.99@gmail.com>
2022-11-26 19:40:07 +0100

    Fixed scalarization

--- denoising_diffusion_pytorch/evaluation.py
+++ denoising_diffusion_pytorch/evaluation.py
@@ -12,15 +12,15 @@ def iou(predicted, ground_truth, threshold=0.5):
 
     intersection = predicted.logical_and(ground_truth).sum()
     union = predicted.logical_or(ground_truth).sum()
-    return (intersection / union).cpu()
+    return (intersection / union).item()
 
 def dice(predicted, ground_truth, threshold=0.5):
     predicted = (predicted >= threshold).int()
     ground_truth = (ground_truth >= threshold).int()
-    return dice_score(predicted, ground_truth).cpu()
+    return dice_score(predicted, ground_truth).item()
 
 def ssim(predicted, ground_truth, *args, **kwargs):
-    return structural_similarity_index_measure(predicted, ground_truth).cpu()
+    return structural_similarity_index_measure(predicted, ground_truth).item()
 
 EVAL_FUNCTIONS = {
     IOU_METRIC: iou,
