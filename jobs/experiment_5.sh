9c189de4245198238edbb6a07bb095e83fbc4d44
Ivaylo Zhelev <ivaylo.zhelev.99@gmail.com>
2023-01-07 21:12:41 +0100

    Updated Python and CUDA versions in the sbatches

--- jobs/experiment_5.sh
+++ jobs/experiment_5.sh
@@ -3,15 +3,14 @@
 #SBATCH --time=3-00:00
 #SBATCH --mem=4000
 #SBATCH --job-name=ddpm_segmentation_experiment_5
-#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=i.zhelev@student.rug.nl
 #SBATCH --output=job-ddpm_segmentation_experiment_5.log
 
 
 module purge
-module load Python/3.6.4-foss-2018a
-module load CUDA/9.1.85
-module load Boost/1.66.0-foss-2018a-Python-3.6.4
+module load Python/3.8.6-GCCcore-10.2.0
+module load CUDA/10.1.243-GCC-8.3.0
 
 source /data/s3782255/.envs/diffusion/bin/activate
 
