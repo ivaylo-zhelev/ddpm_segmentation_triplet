240962f375045e209ef049a488f4e453dbf3e6a7
Ivaylo Zhelev <ivaylo.zhelev.99@gmail.com>
2023-01-07 20:45:10 +0100

    Batch jobs added

---
+++ jobs/experiment_4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=4
+#SBATCH --time=3-00:00
+#SBATCH --mem=4000
+#SBATCH --job-name=ddpm_segmentation
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=i.zhelev@student.rug.nl
+#SBATCH --output=job-ddpm_segmentation.log
+
+
+module purge
+module load Python/3.6.4-foss-2018a
+module load CUDA/9.1.85
+module load Boost/1.66.0-foss-2018a-Python-3.6.4
+
+source /data/s3782255/.envs/diffusion/bin/activate
+
+python /home/s3782255/segmentation_ddpm/ddpm_segmentation_triplet/experiments/training_script.py --config-file=/home/s3782255/segmentation_ddpm/ddpm_segmentation_triplet/configs/experiment_4.yaml
