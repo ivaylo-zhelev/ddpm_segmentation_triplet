75e1209d953c67e65682aeef4d0c196818409861
Ivaylo Zhelev <ivaylo.zhelev.99@gmail.com>
2023-01-23 17:46:59 +0100

    Reversed the change to the time-dependent loss

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
@@ -854,7 +854,8 @@ class GaussianDiffusionSegmentationMapping(GaussianDiffusionBase):
                             reduction='none')
         loss = reduce(loss, 'b ... -> b (...)', 'mean')
 
-        loss = loss * extract(self.p2_loss_weight, t, loss.shape)
+        if not self.is_loss_time_dependent:
+            loss = loss * extract(self.p2_loss_weight, t, loss.shape)
         return loss.mean()
 
     def forward(self, sample_pair, *args, **kwargs):
